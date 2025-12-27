71c44caa2d9d50779efdb19f0ac1b18e9e894b62
Michał Bartkowiak <bartmichu@gmail.com>
2025-12-27 17:12:05 +0100

    Use raw mode

--- scripts/list-sanoid-holds.sh
+++ scripts/list-sanoid-holds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/sbin/zfs list -H -o name -t snapshot | /usr/bin/grep autosnap | while read SNAPSHOT; do
+/usr/sbin/zfs list -H -o name -t snapshot | /usr/bin/grep autosnap | while read -r SNAPSHOT; do
   HOLDS=$(/usr/sbin/zfs holds -H "$SNAPSHOT")
 
   if [ -n "$HOLDS" ]; then
